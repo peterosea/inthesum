0ea8ccac8fa93797435b2bc3d71371e365e7c49b
rihno1 <redwoodweb@naver.com>
2022-03-15 13:17:18 +0900

    title update
    title update

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Robocare</title>
+    <title>inthesum</title>
     <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous"></script>
     <!-- peterosea CSS Style -->
