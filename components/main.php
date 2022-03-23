f54baaa1f116ee27c91100d721c6b946387a3673
rihno1 <redwoodweb@naver.com>
2022-03-24 07:35:04 +0900

    main section 추가
    main section 추가

---
+++ components/main.php
@@ -0,0 +1,97 @@
+
+<div class="main-contents">
+    <section>
+      <div class="inner">
+          <div class="box-title">
+            <div class="box-inner">
+              <img src="./public/img/img-withbts-title@3x.png" alt="with BTS">
+              <p>BTS가 개발에 직접 참여한 인더섬 업데이트 소식</p>
+            </div>
+            <a href="#">arrow btn</a>
+          </div>
+          <div class="box-items-wrap">
+            <div class="inner">
+              <div class="items">
+                <div class="items-inner">
+                  <div class="title">뷔
+                    <span class="icon new">new</span>
+                    <span class="time">10분전</span>
+                  </div>
+                  <p>훗~ 이번에는 인더섬의 과일로 카테일을 만들 수 있는 칵테일바를 오픈했어요, 무알콜 칵테일도 있으니 걱정은 놉!</p>
+                </div>
+              </div>
+              <div class="items">
+                <div class="items-inner">
+                  <div class="title">지민
+                    <span class="icon new">new</span>
+                    <span class="time">2시간전</span>
+                  </div>
+                  <p>훗~ 이번에는 인더섬의 과일로 카테일을 만들 수 있는 칵테일바를 오픈했어요, 무알콜 칵테일도 있으니 걱정은 놉!</p>
+                </div>
+              </div>
+              <div class="items">
+                <div class="items-inner">
+                  <div class="title">슈가
+                    <span class="icon new">new</span>
+                    <span class="time">03.21</span>
+                  </div>
+                  <p>훗~ 이번에는 인더섬의 과일로 카테일을 만들 수 있는 칵테일바를 오픈했어요, 무알콜 칵테일도 있으니 걱정은 놉!</p>
+                </div>
+              </div>
+            </div>
+          </div>
+      </div>
+    </section>
+    <section>
+      <div class="inner">
+          <div class="box-title">
+            <div class="box-inner">
+              <img src="./public/img/img-madebyarmy-title@3x.png" alt="made by army">
+              <div class="title">주간 인더섬 매거진</div>
+              <p>아미와 함께 만드어 나가는 인더섬 이야기</p>
+            </div>
+            <a href="#">arrow btn</a>
+          </div>
+          <div class="box-items-wrap">
+            <div class="inner">
+              <div class="items">
+                <div class="items-inner">
+                  <div class="thumb">
+                    <img src="./public/img/img-magazine1@3x.png" alt="">
+                  </div>
+                  <div class="title">뷔
+                    <span class="icon new">new</span>
+                    <span class="time">10분전</span>
+                  </div>
+                  <p>훗~ 이번에는 인더섬의 과일로 카테일을 만들 수 있는 칵테일바를 오픈했어요, 무알콜 칵테일도 있으니 걱정은 놉!</p>
+                </div>
+              </div>
+              <div class="items">
+                <div class="items-inner">
+                  <div class="thumb">
+                    <img src="./public/img/img-magazine2@3x.png" alt="">
+                  </div>
+                  <div class="title">지민
+                    <span class="icon new">new</span>
+                    <span class="time">2시간전</span>
+                  </div>
+                  <p>훗~ 이번에는 인더섬의 과일로 카테일을 만들 수 있는 칵테일바를 오픈했어요, 무알콜 칵테일도 있으니 걱정은 놉!</p>
+                </div>
+              </div>
+              <div class="items">
+                <div class="items-inner">
+                  <div class="thumb">
+                    <img src="./public/img/img-magazine3@3x.png" alt="">
+                  </div>
+                  <div class="title">슈가
+                    <span class="icon new">new</span>
+                    <span class="time">03.21</span>
+                  </div>
+                  <p>훗~ 이번에는 인더섬의 과일로 카테일을 만들 수 있는 칵테일바를 오픈했어요, 무알콜 칵테일도 있으니 걱정은 놉!</p>
+                </div>
+              </div>
+            </div>
+          </div>
+      </div>
+    </section>
+</div>
--- layout.php
+++ layout.php
@@ -15,53 +15,55 @@ include 'header.php';
       <div id="menu-wrap" class="inner">
         <!-- close menu btn -->
         <a href="#" class="gnb-toggle-btn close"></a>
-        <div class="logo">
-          <a href="/">
-            <img src="./public/img/logo-inthesum-small@3x.png" alt="inthesum logo">
-          </a>
-        </div>
-        <nav id="gnb">
-          <ul>
-            <li>
-              <a href="#1">개임소개</a>
-            </li>
-            <li>
-              <a href="#2">인더섬 개발일지</a>
-            </li>
-            <li>
-              <a href="#">made by ARMY</a>
-            </li>
-            <li>
-              <a href="#">커뮤니티</a>
-            </li>
-          </ul>
-        </nav>
         <!-- pc 버전 -->
-        <div class="right-menu">
-          <ul>
-            <li>
-              <a href="#">
-                <img src="./public/img/icon-gnb-my@3x.png" alt="account img">
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <img src="./public/img/icon-logout@3x.png" alt="logout img">
-              </a>
-            </li>
-            <li>
-              <div class="lang">
-                <select>
-                  <option value="KR">KR</option>
-                  <option value="JP">JP</option>
-                  <option value="US">US</option>
-                </select>
-              </div>
-            </li>
-          </ul>
+        <div class="wrap-box">
+          <div class="logo">
+            <a href="/">
+              <img src="./public/img/logo-inthesum-small@3x.png" alt="inthesum logo">
+            </a>
+          </div>
+          <nav id="gnb">
+            <ul>
+              <li>
+                <a href="#1">개임소개</a>
+              </li>
+              <li>
+                <a href="#2">인더섬 개발일지</a>
+              </li>
+              <li>
+                <a href="#">made by ARMY</a>
+              </li>
+              <li>
+                <a href="#">커뮤니티</a>
+              </li>
+            </ul>
+          </nav>
+          <div class="right-menu">
+            <ul>
+              <li>
+                <a href="#">
+                  <img src="./public/img/icon-gnb-my@3x.png" alt="account img">
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img src="./public/img/icon-logout@3x.png" alt="logout img">
+                </a>
+              </li>
+              <li>
+                <div class="lang">
+                  <select>
+                    <option value="KR">KR</option>
+                    <option value="JP">JP</option>
+                    <option value="US">US</option>
+                  </select>
+                </div>
+              </li>
+            </ul>
+          </div>
         </div>
         <!-- moblie 버전 -->
-        <div class="bottom-menu" style="display: none;">
+        <div class="bottom-menu">
           <ul>
             <li>
               <a href="#">
@@ -77,15 +79,15 @@ include 'header.php';
         </div>
       </div>
     </header>
-    <div class="main-banner">
-      <img src="./public/img/img-main-top@3x.png" alt="mainbanner01">
-    </div>
+    <div class="main-banner"></div>
     <!-- content : 콘텐츠 영역 -->
     <div class="content">
         <section class="content_inner">
             <?php
                 // 01. jQgrid 테이블
-                include 'components/test.php';
+                // include 'components/test.php';
+                // main contents
+                include 'components/main.php';
             ?>
         </section>
         <!-- //.content_inner -->
