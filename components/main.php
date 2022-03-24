e11654c8b1a6391e236bce7442c6f0f727480561
rihno1 <redwoodweb@naver.com>
2022-03-24 22:46:22 +0900

    update main section
    update main section

--- components/main.php
+++ components/main.php
@@ -6,36 +6,39 @@
             <div class="box-inner">
               <img src="./public/img/img-withbts-title@3x.png" alt="with BTS">
               <p>BTS가 개발에 직접 참여한 인더섬 업데이트 소식</p>
-              <a href="#">arrow btn</a>
+              <a href="#"><img src="./public/img/icon-more-arrow@3x.png" alt="more view"></a>
             </div>
           </div>
           <div class="box-items-wrap">
             <div class="inner">
               <div class="items">
-                <div class="items-inner">
+                <div class="items-inner type-a">
                   <div class="title">뷔
-                    <span class="icon new">new</span>
+                    <span class="icon new"><img src="./public/img/new@3x.png" alt="new"></span>
                     <span class="time">10분전</span>
                   </div>
                   <p>훗~ 이번에는 인더섬의 과일로 카테일을 만들 수 있는 칵테일바를 오픈했어요, 무알콜 칵테일도 있으니 걱정은 놉!</p>
+                  <img class="charactor-img" src="./public/img/img-bts-v@3x.png" alt="슈가 캐릭터">
                 </div>
               </div>
               <div class="items">
-                <div class="items-inner">
+                <div class="items-inner type-a">
                   <div class="title">지민
-                    <span class="icon new">new</span>
+                    <span class="icon new"><img src="./public/img/new@3x.png" alt="new"></span>
                     <span class="time">2시간전</span>
                   </div>
                   <p>훗~ 이번에는 인더섬의 과일로 카테일을 만들 수 있는 칵테일바를 오픈했어요, 무알콜 칵테일도 있으니 걱정은 놉!</p>
+                  <img class="charactor-img" src="./public/img/img-bts-jimin@3x.png" alt="지민 캐릭터">
                 </div>
               </div>
               <div class="items">
-                <div class="items-inner">
+                <div class="items-inner type-a">
                   <div class="title">슈가
-                    <span class="icon new">new</span>
+                    <span class="icon new"><img src="./public/img/new@3x.png" alt="new"></span>
                     <span class="time">03.21</span>
                   </div>
                   <p>훗~ 이번에는 인더섬의 과일로 카테일을 만들 수 있는 칵테일바를 오픈했어요, 무알콜 칵테일도 있으니 걱정은 놉!</p>
+                  <img class="charactor-img" src="./public/img/img-bts-suga@3x.png" alt="슈가 캐릭터">
                 </div>
               </div>
             </div>
@@ -49,45 +52,57 @@
               <img src="./public/img/img-madebyarmy-title@3x.png" alt="made by army">
               <div class="title">주간 인더섬 매거진</div>
               <p>아미와 함께 만드어 나가는 인더섬 이야기</p>
-              <a href="#">arrow btn</a>
+              <a href="#"><img src="./public/img/icon-more-arrow@3x.png" alt="more view"></a>
             </div>
           </div>
           <div class="box-items-wrap">
             <div class="inner">
               <div class="items">
-                <div class="items-inner">
+                <div class="items-inner type-b">
                   <div class="thumb">
                     <img src="./public/img/img-magazine1@3x.png" alt="">
                   </div>
-                  <div class="title">뷔
-                    <span class="icon new">new</span>
-                    <span class="time">10분전</span>
+                  <div class="title">
+                    <span class="vol">Vol.3</span>
+                    <p class="name">Fishing Life</p>
+                    <span class="date">2022.03.02</span>
                   </div>
-                  <p>훗~ 이번에는 인더섬의 과일로 카테일을 만들 수 있는 칵테일바를 오픈했어요, 무알콜 칵테일도 있으니 걱정은 놉!</p>
+                  <div class="spec">
+                    <p>커버스토리: 인더섬에서 바다낚시하기</p>
+                    <p>주요기사: 칵테일 만들기</p>
+                  </div>
                 </div>
               </div>
               <div class="items">
-                <div class="items-inner">
+                <div class="items-inner type-b">
                   <div class="thumb">
                     <img src="./public/img/img-magazine2@3x.png" alt="">
                   </div>
-                  <div class="title">지민
-                    <span class="icon new">new</span>
-                    <span class="time">2시간전</span>
+                  <div class="title">
+                    <span class="vol">Vol.2</span>
+                    <p class="name">Healing Camp</p>
+                    <span class="date">2022.02.23</span>
                   </div>
-                  <p>훗~ 이번에는 인더섬의 과일로 카테일을 만들 수 있는 칵테일바를 오픈했어요, 무알콜 칵테일도 있으니 걱정은 놉!</p>
+                  <div class="spec">
+                    <p>커버스토리: 베이스캠프 건설</p>
+                    <p>주요기사: 요리 레시피</p>
+                  </div>
                 </div>
               </div>
               <div class="items">
-                <div class="items-inner">
+                <div class="items-inner type-b">
                   <div class="thumb">
                     <img src="./public/img/img-magazine3@3x.png" alt="">
                   </div>
-                  <div class="title">슈가
-                    <span class="icon new">new</span>
-                    <span class="time">03.21</span>
+                  <div class="title">
+                    <span class="vol">Vol.1</span>
+                    <p class="name">Grand Open</p>
+                    <span class="date">2022.02.16</span>
                   </div>
-                  <p>훗~ 이번에는 인더섬의 과일로 카테일을 만들 수 있는 칵테일바를 오픈했어요, 무알콜 칵테일도 있으니 걱정은 놉!</p>
+                  <div class="spec">
+                    <p>커버스토리: 전세계 전격 오픈</p>
+                    <p>주요기사: 튜토리얼</p>
+                  </div>
                 </div>
               </div>
             </div>
