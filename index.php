eca4ab07fec9dc679f437fc06c2f55af71ade7d2
Hansanghyeon <999@hyeon.pro>
2022-03-29 19:10:38 +0900

    feat: 데모사이트 설정 & 설정파일

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
+  <style>
+    [x-cloak] {
+      display: none;
+    }
+  </style>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/alpinejs/2.3.0/alpine.js" defer=""></script>
+  <title>인더섬</title>
+</head>
+
+<body class="relative">
+  <div class="container mx-auto py-[90px] min-h-screen flex flex-col justify-center items-center">
+    <div class="prose">
+      <h1>퍼블리싱 사이트맵</h1>
+      <ul>
+        <li>
+          <a href="/layout.php">/layout.php</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</body>
+
+</html>
