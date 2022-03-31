0d2c865ae26ab91de7c193a81aacd34e27d03f38
peter <50910448+Seunghan-Lee@users.noreply.github.com>
2022-03-31 22:29:01 +0900

    feat: 커뮤니티 - 공지사항 (peterosea/inthesum#12)

--- index.php
+++ index.php
@@ -26,6 +26,9 @@
         <li>
           <span class="inline-block py-[3px] px-[10px] bg-blue-500 rounded-full mr-[8px] text-sm font-bold text-white">커뮤니티</span><a href="/community.php">/community.php</a>
         </li>
+        <li>
+          <span class="inline-block py-[3px] px-[10px] bg-blue-500 rounded-full mr-[8px] text-sm font-bold text-white">커뮤니티/공지사항</span><a href="/community-news.php">/community-news.php</a>
+        </li>
       </ul>
     </div>
   </div>
