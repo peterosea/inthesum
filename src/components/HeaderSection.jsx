55a205d994058a8da9fa51c5974d0b87e86d3800
Hansanghyeon <999@hyeon.pro>
2022-04-09 07:05:08 +0900

    feat: HeaderSection 컴포넌트 인자 변경 & 추가

--- src/components/HeaderSection.jsx
+++ src/components/HeaderSection.jsx
@@ -1,32 +1,43 @@
 import React from 'react';
+import classnames from 'classnames';
 
 // 임시 기본 값
-const aTitle = '자주 묻는 질문';
+const aTitle = () => (
+  <h1 className="text-[36px] mb-[15px] font-TmoneyRoundWind font-bold leading-[0.69]">
+    자주 묻는 질문
+  </h1>
+);
 const aContent = () => <p>질문 전에 확인하시면 빠르게 해결하실 수도 있어요</p>;
 
 /**
  *
- * @property {string} title
+ * @property {JSX.Element} title
  * @property {JSX.Element} content
  */
 const HeaderSection = ({
   title = aTitle,
   content = aContent,
   arrow = false,
-}) => (
-  <div className="flex gap-x-[30px] justify-between font-Pretendard">
-    <div>
-      <h1 className="text-[36px] mb-[15px] font-TmoneyRoundWind font-bold leading-[0.69]">
-        {title}
-      </h1>
-      <div className="text-[18px]">{content()}</div>
-    </div>
-    {arrow ?? (
+  className,
+}) => {
+  return (
+    <div
+      className={classnames(
+        'flex gap-x-[30px] justify-between font-Pretendard',
+        className,
+      )}
+    >
       <div>
-        <i className="icon-arrow2 text-[31px] rotate-180 inline-block"></i>
+        {title()}
+        <div className="text-[18px]">{content()}</div>
       </div>
-    )}
-  </div>
-);
+      {arrow ?? (
+        <div>
+          <i className="icon-arrow2 text-[31px] rotate-180 inline-block"></i>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default HeaderSection;
