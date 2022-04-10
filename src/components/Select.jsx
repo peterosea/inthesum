5658502ba389065288587478ea2db2393ba89284
Hansanghyeon <999@hyeon.pro>
2022-04-10 15:14:55 +0900

    :sparkles: select ui 변경

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -1,21 +1,130 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
+import { useOnClickOutside } from 'usehooks-ts';
+import { Transition } from '@headlessui/react';
+import classNames from 'classnames';
 
-const Select = ({ options }) => {
-  return (
-    <>
-      <label htmlFor="tabs" className="sr-only">
-        Select a tab
-      </label>
-      <select
-        id="tabs"
-        name="tabs"
-        className="leading-none block w-full focus:outline-none focus:shadow-none focus:ring-0 focus:border-primary border-[#e6eaf2] font-Pretendard text-[14px] pl-[26px] py-[21px] text-[#333] focus:text-black"
+const DefButton = ({ children }) => (
+  <button
+    type="button"
+    className="bg-white relative w-full border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-primary focus:border-primary sm:text-sm"
+    aria-haspopup="listbox"
+    aria-expanded="true"
+    aria-labelledby="listbox-label"
+  >
+    <span className="block truncate">{children}</span>
+    <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+      {/* <!-- Heroicon name: solid/selector --> */}
+      <svg
+        className="h-5 w-5 text-gray-400"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+        aria-hidden="true"
       >
-        {options.map((option, index) => (
-          <option key={`index-${index}`}>{option}</option>
-        ))}
-      </select>
-    </>
+        <path
+          fillRule="evenodd"
+          d="M10 3a1 1 0 01.707.293l3 3a1 1 0 01-1.414 1.414L10 5.414 7.707 7.707a1 1 0 01-1.414-1.414l3-3A1 1 0 0110 3zm-3.707 9.293a1 1 0 011.414 0L10 14.586l2.293-2.293a1 1 0 011.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z"
+          clipRule="evenodd"
+        />
+      </svg>
+    </span>
+  </button>
+);
+/**
+ *
+ * @property {array} options
+ * @property {JSX.Element} button <button>option name</button>
+ */
+const Select = ({ options, button = DefButton }) => {
+  const ref = useRef(null);
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectId, setSelectId] = useState(options[0].id);
+  const [selectTitle, setSelectTitle] = useState(options[0].title);
+
+  useOnClickOutside(ref, () => setIsOpen(false));
+
+  const handleOptionSelect = (value, title) => {
+    setIsOpen(false);
+    setSelectId(value);
+    setSelectTitle(title);
+  };
+
+  const Button = ({ children }) =>
+    React.cloneElement(button({ children }), {
+      onClick: () => setIsOpen(!isOpen),
+    });
+
+  return (
+    <div ref={ref}>
+      <label className="sr-only">Select a tab</label>
+      <div className="mt-1 relative">
+        <Button>{selectTitle}</Button>
+        <Transition
+          show={isOpen}
+          leave="transition ease-in duration-100"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+          className="absolute z-10 mt-1 w-full bg-white shadow-lg max-h-60 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm"
+        >
+          <ul
+            tabIndex="-1"
+            role="listbox"
+            aria-labelledby="listbox-label"
+            aria-activedescendant="listbox-option-3"
+          >
+            {options.map(({ id, title }) => {
+              const isSelect = selectId === id;
+              return (
+                <li
+                  key={id}
+                  className={classNames(
+                    'text-gray-900 cursor-default select-none relative py-2 pl-3 pr-9 group',
+                    'hover:text-white hover:bg-primary',
+                    {
+                      'font-bold': isSelect,
+                    },
+                  )}
+                  role="option"
+                  onClick={() => handleOptionSelect(id, title)}
+                >
+                  <span
+                    className={classNames(
+                      'block truncate',
+                      {
+                        'font-semibold': isSelect,
+                      },
+                      { 'font-normal': !isSelect },
+                    )}
+                  >
+                    {title}
+                  </span>
+                  <span
+                    className={classNames(
+                      'text-primary absolute inset-y-0 right-0 flex items-center pr-4',
+                      { 'group-hover:text-white': isSelect },
+                      { 'opacity-0': !isSelect },
+                    )}
+                  >
+                    <svg
+                      className="h-5 w-5"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </span>
+                </li>
+              );
+            })}
+          </ul>
+        </Transition>
+      </div>
+    </div>
   );
 };
 
