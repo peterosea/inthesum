23f70adbd04a07bf5a74dbb2b305b8c74056c1a8
Hansanghyeon <999@hyeon.pro>
2022-04-11 06:35:32 +0900

    feat: 메인페이지 반응형

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -10,19 +10,21 @@ import MagazineSlide from '../components/home/MagazineSlide';
 import CharacterSlide from '../components/home/CharacterSlide';
 import MainBannerSlide from '../components/home/MainBannerSlide';
 import Card from '../components/Card';
+import HeaderSection from '../components/HeaderSection';
 import { card } from './data';
 
 const Main = () => {
   return (
     <>
-      <section className="py-[90px]">
+      <section className="mt-[38px] mb-[60px] xl:my-[90px]">
         <div className="border-b border-[#ddd]">
           <div className="xl:container mx-auto">
             <div className="grid xl:grid-cols-[270px,auto]">
               <div className="xl:min-h-[490px] relative">
-                <div className="flex xl:flex-col justify-between">
-                  <div className="w-[160px]">
+                <div className="flex xl:flex-col justify-between #xl:px-[30px] #xl:container #xl:mx-auto">
+                  <div>
                     <img
+                      className="h-[55px] xl:h-[93px]"
                       src="/public/img/img-withbts-title@3x.png"
                       alt="with BTS"
                     />
@@ -30,11 +32,11 @@ const Main = () => {
                       <p>BTS가 개발에 직접 참여한 인더섬 업데이트 소식</p>
                     </div>
                   </div>
-                  <a href="#">
+                  <a href="#" className="#xl:self-center">
                     <img
                       src="/public/img/icon-more-arrow@3x.png"
                       alt="more view"
-                      className="w-[31px]"
+                      className="w-[16px] xl:w-[31px]"
                     />
                   </a>
                 </div>
@@ -47,7 +49,7 @@ const Main = () => {
           </div>
         </div>
       </section>
-      <section className="overflow-hidden my-[90px]">
+      <section className="overflow-hidden my-[64px] xl:my-[90px]">
         <div className="container mx-auto relative">
           <div className="magazine__header xl:h-full xl:absolute top-0 left-[var(--container-px)] z-10 bg-gradient-to-r from-white via-white">
             <div className="flex xl:flex-col justify-between">
@@ -80,14 +82,17 @@ const Main = () => {
           </div>
         </div>
       </section>
-      <section className="my-[120px]">
+      <section className="my-[64px] xl:my-[120px]">
         <div className="container mx-auto">
-          <div className="grid xl:grid-cols-2 gap-[20px]">
+          <div className="grid xl:grid-cols-2 gap-x-[20px] #xl:mx-full">
             <a
               href="#"
-              className="relative rounded-[12px] min-h-[260px] flex p-[50px] pr-[280px] bg-[#EDEDFD]"
+              className={classnames(
+                'relative min-h-[260px] flex py-[42px] px-[30px] bg-[#EDEDFD] items-center flex-wrap gap-y-[35px] justify-end',
+                'xl:rounded-[12px] xl:p-[50px] xl:justify-between',
+              )}
             >
-              <div>
+              <div className="#xl:w-full">
                 <h4 className="text-[#2e0064] font-bold">개발 PD 노트</h4>
                 <div className="mt-[8px] text-[26px] text-primary font-bold">
                   인더섬 with BTS
@@ -101,50 +106,54 @@ const Main = () => {
                 </div>
               </div>
               <img
+                className="h-[150px] w-auto"
                 src="/public/img/banner-pd-note-img@3x.png"
                 alt=""
-                className="absolute right-[40px] top-1/2 -translate-y-1/2 w-[30%]"
               />
             </a>
             <a
               href="#"
-              className="relative rounded-[12px] min-h-[260px] flex p-[50px] pr-[280px] bg-[#fdf4ff]"
+              className={classnames(
+                'relative  min-h-[260px] flex  items-center py-[42px] px-[30px]  bg-[#fdf4ff] flex-wrap gap-y-[35px] justify-end',
+                'xl:rounded-[12px] xl:p-[50px] xl:justify-between',
+              )}
             >
-              <div>
-                <h4 className="text-[#4d005d] font-bold">개발 PD 노트</h4>
-                <div className="mt-[8px] text-[26px] text-[#7f0198] font-bold">
-                  인더섬 with BTS
+              <div className="#xl:w-full">
+                <h4 className="text-[#4d005d] font-bold">
+                  made by ARMY - BTS 의상실
+                </h4>
+                <div className="mt-[8px] text-[36px] xl:text-[26px] text-[#7f0198] font-bold">
+                  RM의 S/S 신상 의상 투표
                   <br />
-                  전세계 동시 오픈!!
+                  여러분의 PICK은?
                 </div>
                 <div className="text-[#360041] mt-[20px]">
-                  인더섬 2022년 4월 1일 GMT 00:00에
+                  여러분의 감성으로 투표해주세요!
                   <br />
-                  전세계 양대 마켓에서 동시 오픈합니다.
+                  선택받은 코스튬은 게임에 실제로 반영됩니다!
                 </div>
               </div>
               <img
                 src="/public/img/banner-bts-cloth-img@3x.png"
                 alt=""
-                className="absolute right-[40px] top-1/2 -translate-y-1/2 w-[30%]"
+                className="w-auto h-[135px]"
               />
             </a>
           </div>
         </div>
       </section>
-      <section className="my-[120px]">
+      <section className="my-[64px] xl:my-[120px]">
         <div className="container mx-auto">
-          <div>
-            <h1 className="text-[36px] font-bold leading-[0.69]">
-              인더섬 뉴스
-            </h1>
-            <div className="text-[18px] mt-[16px]">
+          <HeaderSection
+            title={() => <h1>인더섬 뉴스</h1>}
+            content={() => (
               <p>인더섬에서는 과연 어떠한 일이 일어나고 있고 일어날까요?</p>
-            </div>
-          </div>
+            )}
+          />
           <div
             className={classnames(
-              'mt-[60px]',
+              'mt-[42px] mx-full',
+              'xl:mt-[60px]',
               'grid grid-cols-1 gap-y-[30px]',
               'xl:grid-cols-2 xl:gap-[40px] xl:gap-y-[94px]',
             )}
@@ -164,7 +173,7 @@ export default () => {
     <>
       <Header />
       <MainBannerSlide />
-      <div className="font-Pretendard mb-[120px]">
+      <div className="font-Pretendard">
         <Main />
       </div>
       <Footer />
