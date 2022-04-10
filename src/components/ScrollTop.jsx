93eed2991183e05ca0451b643d8f4a2a9648cd28
Hansanghyeon <999@hyeon.pro>
2022-04-10 20:05:04 +0900

    feat: 스크롤탑 버튼 반응형

--- src/components/ScrollTop.jsx
+++ src/components/ScrollTop.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 const ScrollTop = () => {
   return (
-    <div className="relative h-px bg-[#e6eaf2]">
+    <div className="relative h-px xl:bg-[#e6eaf2]">
       <a
         href="#"
-        className="w-[80px] h-[80px] bg-[#f4f6fa] flex items-center justify-center absolute right-0 top-1/2 -translate-y-1/2 rounded-full"
+        className="w-[50px] h-[50px] xl:w-[80px] xl:h-[80px] bg-[#f4f6fa] flex items-center justify-center absolute right-0 top-1/2 -translate-y-1/2 rounded-full"
       >
-        <i className="icon-arrow rotate-180 text-[28px] text-[#c3cad5]"></i>
+        <i className="icon-arrow rotate-180 text-[12px] xl:text-[28px] text-[#c3cad5]"></i>
       </a>
     </div>
   );
