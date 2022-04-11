b1b2026dca498037c878a5968ee3e6ab63521b74
Hansanghyeon <999@hyeon.pro>
2022-04-11 12:54:49 +0900

    feat: 데모사이트

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,16 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import Pages from 'vite-plugin-pages';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    hmr: {
+      host: '0.0.0.0',
+      port: 3000,
+      watch: {
+        usePolling: true
+      }
+    },
+  },
   plugins: [
     react(),
     Pages({
