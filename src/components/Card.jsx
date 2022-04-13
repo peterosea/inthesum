528fd5807690101f741bfa52c6fdc73ad450bd46
Hansanghyeon <999@hyeon.pro>
2022-04-13 21:21:27 +0900

    jam4 - 메인 포스트 카드 본문 폰트 컬러 & 두께

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -49,8 +49,8 @@ const Card = ({ pin = false, data = cardData }) => {
             </a>
             <div
               className={classnames(
-                'text-[12px] text-[#54575d] mt-[5px] line-clamp-2',
-                'xl:mt-[12px] xl:text-[16px] xl:line-clamp-3',
+                'text-[12px] text-[#6f737a] mt-[5px] line-clamp-2 font-normal',
+                'xl:mt-[12px] xl:text-[16px] xl:line-clamp-3 xl:text-[#54575d]',
               )}
             >
               {content()}
