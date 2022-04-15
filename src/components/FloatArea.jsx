358c66d7201efba16fe01b62a78fa451b7f4605f
Hansanghyeon <999@hyeon.pro>
2022-04-15 17:50:52 +0900

    feat: 하단 observer float 사라짐

--- src/components/FloatArea.jsx
+++ src/components/FloatArea.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
+import useShrink from '../hook/useShrink';
+import { Transition } from '@headlessui/react';
+import cx from 'classnames';
+// components
 import ScrollTop from './ScrollTop';
 import GooglePlayBtn from './Btn/GooglePlayBtn';
 import AppStoreBtn from './Btn/AppStoreBtn';
-import useShrink from '../hook/useShrink';
-import { Transition } from '@headlessui/react';
-
-import cx from 'classnames';
 
 const FloatArea = () => {
-  const isShrink = useShrink(window.innerHeight * 0.8 - 110);
+  const topShrink = useShrink(window.innerHeight * 0.8 - 110);
+  const bottomShrink = useShrink(document.body.scrollHeight - 800);
+  let Shrink = false;
+  if (topShrink) {
+    Shrink = true;
+    if (bottomShrink) {
+      Shrink = false;
+    }
+  }
+
   return (
     <Transition
-      show={isShrink}
+      show={Shrink}
       leave="transition ease-in duration-100"
       leaveFrom="opacity-100"
       leaveTo="opacity-0"
@@ -19,8 +28,8 @@ const FloatArea = () => {
     >
       <div
         className={cx(
-          'absolute flex gap-x-[10px] gap-y-[20px] right-0 bottom-[30px]',
-          ' hug:left-full hug:translate-x-[60px] hug:right-auto hug:bottom-[130px] hug:flex-col',
+          'absolute flex gap-x-[10px] gap-y-[20px] right-0 bottom-[30px] #hug:pr-cpx',
+          'hug:left-full hug:translate-x-[60px] hug:right-auto hug:bottom-[130px] hug:flex-col',
         )}
       >
         <AppStoreBtn />
