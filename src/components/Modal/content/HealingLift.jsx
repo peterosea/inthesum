4d1265e86bc2a2dd91cb0f356ae57b98970905f2
Hansanghyeon <999@hyeon.pro>
2022-04-23 11:53:54 +0900

    feat: 메인팝업 type3 max 사이즈

--- src/components/Modal/content/HealingLift.jsx
+++ src/components/Modal/content/HealingLift.jsx
@@ -10,33 +10,7 @@ const HealingLift = () => {
         alt="with BTS"
         className="absolute h-full w-auto max-w-none left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"
       />
-      <div className="relative h-[450px] flex items-center justify-center flex-col text-white text-center">
-        <img
-          src="/img/bts_logo.png"
-          srcSet="/img/bts_logo@2x.png 2x,
-                  /img/bts_logo@3x.png 3x"
-          alt="BTS Logo"
-          className="w-[56px]"
-        />
-        <div className="uppercase font-TmoneyRoundWind font-extrabold text-[30px] mt-[21px]">
-          Healing island life
-        </div>
-        <div className="mt-[7px] font-Pretendard">
-          3D Puzzle Game with BTS character
-          <br />
-          and space decoration
-        </div>
-        <div className="mt-[25px]">
-          <a href="#">
-            <img
-              src="/img/btn-googleplay.png"
-              srcSet="/img/btn-googleplay@2x.png 2x,
-                      /img/btn-googleplay@3x.png 3x"
-              className="h-[40px]"
-            />
-          </a>
-        </div>
-      </div>
+      <div className="relative h-[450px] flex items-center justify-center flex-col text-white text-center"></div>
     </>
   );
 };
