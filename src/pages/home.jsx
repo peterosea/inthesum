9821ec9e626487402786dc43f9d10bf02fba0230
Hansanghyeon <999@hyeon.pro>
2022-04-27 18:51:43 +0900

    fix: 커밍순 배너 제거

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -242,92 +242,76 @@ const Main = () => {
   return (
     <>
       <section className="mt-[38px] mb-[60px] xl:my-[90px]">
-        <div className="">
+        <div className="border-b border-[#ddd]">
           <div className="xl:container mx-auto">
-            <div className="w-full h-full flex items-center relative">
-              <div className="text-white pl-[60px] h-[380px] flex items-center relative z-10 w-[500px] bg-gradient-to-l to-[rgba(0,0,0,0.3)] from-transparent">
-                <div>
-                  <div className="relative">
+            <div className="grid xl:grid-cols-[270px,auto]">
+              <div className="xl:min-h-[490px] relative">
+                <div className="flex xl:flex-col justify-between #xl:px-[30px] #xl:container #xl:mx-auto">
+                  <div className="max-w-[160px]">
                     <img
-                      src="/img/img-withbts-title-white.png"
-                      srcSet="/img/img-withbts-title-white@2x.png 2x,
-                                  /img/img-withbts-title-white@3x.png 3x"
+                      src="/img/img-withbts-title.png"
+                      srcSet="/img/img-withbts-title@2x.png 2x,
+                            /img/img-withbts-title@3x.png 3x"
                       alt="with BTS"
                       className="h-[55px] xl:h-[93px] w-auto"
                     />
+                    <div className="my-[30px] hidden xl:block">
+                      <p>BTS가 개발에 직접 참여한 인더섬 업데이트 소식</p>
+                    </div>
+                  </div>
+                  <a href="#" className="#xl:self-center">
                     <img
-                      src="/img/coming-soon.png"
-                      srcSet="/img/coming-soon@2x.png 2x,
-                                /img/coming-soon@3x.png 3x"
-                      alt="comming soon"
-                      className="xl:h-[38px] w-auto absolute bottom-full left-0"
+                      src="/img/icon-more-arrow@3x.png"
+                      alt="more view"
+                      className="w-[16px] xl:w-[31px]"
                     />
-                  </div>
-                  <div className="mt-[30px] hidden xl:block">
-                    <p>
-                      BTS와 인더섬을 즐기는 방법!
-                      <br />곧 찾아옵니다.
-                    </p>
-                  </div>
+                  </a>
                 </div>
+                <div className="absolute w-[110px] h-px bottom-[-1px] right-0 bg-white"></div>
               </div>
-              <img
-                src="/img/madebyarmi-comingsoon1.png"
-                srcSet="/img/madebyarmi-comingsoon1@2x.png 2x,
-                              /img/madebyarmi-comingsoon1@3x.png 3x"
-                alt="comming soon"
-                className="absolute h-full object-cover"
-              />
+              <div className="overflow-hidden xl:mt-0 mt-[35px] h-[500px]">
+                <CharacterSlide />
+              </div>
             </div>
           </div>
         </div>
-        <div className="mt-[20px]">
-          <div className="xl:container mx-auto">
-            <div className="w-full h-full flex items-center relative">
-              <div className="text-white pl-[60px] h-[380px] flex items-center relative z-10 w-[500px] bg-gradient-to-l to-[rgba(0,0,0,0.3)] from-transparent">
-                <div>
-                  <div className="relative">
-                    <img
-                      src="/img/img-madebyarmy-title-w.png"
-                      srcSet="/img/img-madebyarmy-title-w@2x.png 2x,
-                                  /img/img-madebyarmy-title-w@3x.png 3x"
-                      alt="with BTS"
-                      className="h-[55px] xl:h-[93px] w-auto"
-                    />
-                    <div className="text-[22px] text-white font-TmoneyRoundWind font-extrabold mt-[20px]">
-                      주간 인더섬 매거진
-                    </div>
-                    <img
-                      src="/img/coming-soon.png"
-                      srcSet="/img/coming-soon@2x.png 2x,
-                                /img/coming-soon@3x.png 3x"
-                      alt="comming soon"
-                      className="xl:h-[38px] w-auto absolute bottom-full left-0"
-                    />
-                  </div>
-                  <div className="mt-[30px] hidden xl:block">
-                    <p>
-                      아미가 만들어가는 새로운 이야기!
-                      <br />
-                      기대해주세요!
-                    </p>
-                  </div>
+      </section>
+      <section>
+        <div className="xl:container mx-auto relative">
+          <div
+            className={classnames(
+              'px-cpx py-[90] xl:py-[90px]',
+              'xl:h-full xl:absolute xl:top-0 xl:left-0 xl:z-10',
+            )}
+          >
+            <div className="flex xl:flex-col justify-between">
+              <div className="w-[160px]">
+                <img src="/img/img-madebyarmy-title@3x.png" alt="with BTS" />
+                <div className="text-[22px] font-bold whitespace-nowrap text-justify mt-[20px]">
+                  주간 인더섬 매거진
+                </div>
+                <div className="my-[30px] hidden xl:block">
+                  <p>아미와 함께 만드어 나가는 인더섬 이야기</p>
                 </div>
               </div>
-              <img
-                src="/img/madebyarmi-comingsoon.png"
-                srcSet="/img/madebyarmi-comingsoon@2x.png 2x,
-                              /img/madebyarmi-comingsoon@3x.png 3x"
-                alt="comming soon"
-                className="absolute h-full object-cover"
-              />
+              <a href="#">
+                <img
+                  src="/img/icon-more-arrow@3x.png"
+                  alt="more view"
+                  className="w-[31px]"
+                />
+              </a>
             </div>
+            <div className="absolute w-[110px] h-px bottom-[-1px] right-0 bg-white hidden xl:block"></div>
+          </div>
+          <div className="xl:pl-[270px]">
+            <MagazineSlide />
           </div>
         </div>
       </section>
       <section className="mb-[64px] xl:mb-[120px]">
         <div className="container mx-auto">
-          <div className="grid xl:grid-cols-2 gap-x-[20px] #xl:mx-full">
+          <div className="grid xl:grid-cols-2 gap-x-[20px] #xl:mx-full overflow-hidden">
             <div>
               <PDBanner />
             </div>
