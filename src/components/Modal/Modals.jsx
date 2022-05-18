08b3c630a03def0964fb2f531374693311079924
Hansanghyeon <999@hyeon.pro>
2022-05-18 17:50:08 +0900

    feat(refactoring): 모달 스타일 구조 변경

--- src/components/Modal/Modals.jsx
+++ src/components/Modal/Modals.jsx
@@ -3,6 +3,7 @@ import Modal from '.';
 import IeDeprecated from './content/IeDeprecated';
 import Notice0407 from './content/Notice0407';
 import HealingLift from './content/HealingLift';
+import Login from './content/Login';
 
 const Modals = () => {
   const [helpModal, setHelpModal] = useState(false);
@@ -24,6 +25,7 @@ const Modals = () => {
     type1: false,
     type2: false,
     type3: false,
+    type4: false,
   });
   const openModalType = (type) => {
     const cModalType = Object.assign(modalType);
@@ -45,25 +47,33 @@ const Modals = () => {
   return (
     <>
       <Modal isOpen={helpModal} onClose={() => setHelpModal(false)}>
-        <div className="flex gap-x-[8px]">
-          <button
-            className="inline-block py-[3px] px-[10px] bg-teal-500 rounded-full mr-[8px] text-sm font-semibold text-white"
-            onClick={() => openModalType('type1')}
-          >
-            Modal 1
-          </button>
-          <button
-            className="inline-block py-[3px] px-[10px] bg-teal-500 rounded-full mr-[8px] text-sm font-semibold text-white"
-            onClick={() => openModalType('type2')}
-          >
-            Modal 2
-          </button>
-          <button
-            className="inline-block py-[3px] px-[10px] bg-teal-500 rounded-full mr-[8px] text-sm font-semibold text-white"
-            onClick={() => openModalType('type3')}
-          >
-            Modal 3
-          </button>
+        <div className='bg-[#F4F6FA] p-[40px] pt-[46px] text-left  border-t-[3px] border-primary'>
+          <div className="flex gap-x-[8px]">
+            <button
+              className="inline-block py-[3px] px-[10px] bg-teal-500 rounded-full mr-[8px] text-sm font-semibold text-white"
+              onClick={() => openModalType('type1')}
+            >
+              Modal 1
+            </button>
+            <button
+              className="inline-block py-[3px] px-[10px] bg-teal-500 rounded-full mr-[8px] text-sm font-semibold text-white"
+              onClick={() => openModalType('type2')}
+            >
+              Modal 2
+            </button>
+            <button
+              className="inline-block py-[3px] px-[10px] bg-teal-500 rounded-full mr-[8px] text-sm font-semibold text-white"
+              onClick={() => openModalType('type3')}
+            >
+              Modal 3
+            </button>
+            <button
+              className="inline-block py-[3px] px-[10px] bg-teal-500 rounded-full mr-[8px] text-sm font-semibold text-white"
+              onClick={() => openModalType('type4')}
+            >
+              로그인
+            </button>
+          </div>
         </div>
       </Modal>
       <Modal isOpen={modalType.type1} onClose={() => closeModalType('type1')}>
@@ -82,6 +92,12 @@ const Modals = () => {
         timeClose={true}
       >
         <HealingLift />
+      </Modal>
+      <Modal
+        isOpen={modalType.type4}
+        onClose={() => closeModalType('type4')}
+      >
+        <Login />
       </Modal>
     </>
   );
