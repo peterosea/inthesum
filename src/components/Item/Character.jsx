31e2e093c3658fb41598e100f1aeb797cbc889e7
999 <999@hyeon.pro>
2022-05-19 07:21:06 +0900

    Hansanghyeon/issue70 (#71)

--- src/components/Item/Character.jsx
+++ src/components/Item/Character.jsx
@@ -1,27 +1,6 @@
 import React from 'react';
 import cx from 'classnames';
 
-const Thumbnail = ({ img, type }) => {
-  const ModifyThumbnail = () =>
-    React.cloneElement(img(), {
-      className: 'absolute object-cover',
-    });
-
-  let Cover = () => <></>;
-  if (type === 'video') {
-    Cover = () => (
-      <div className="absolute w-full h-full bg-black bg-opacity-50 flex items-center justify-center">
-        <i className="icon-right-dir text-[30px] text-white" />
-      </div>
-    );
-  }
-  return (
-    <div className="w-[70px] h-[70px] rounded-[20px] overflow-hidden relative">
-      <ModifyThumbnail />
-      <Cover />
-    </div>
-  );
-};
 /**
  *
  * @param {string} name
@@ -31,29 +10,16 @@ const Thumbnail = ({ img, type }) => {
  * @param {JSX.Element} characterImg
  * @returns
  */
-const CharacterItem = ({ name, time, content, thumbnail, characterImg }) => {
-  let Thumbnails = () => <></>;
-  if (thumbnail) {
-    Thumbnails = () => (
-      <div className="flex gap-x-[10px] mt-[20px] items-center">
-        {thumbnail?.map((t, index) => {
-          if (index < 2) return <Thumbnail {...t} key={`index-${index}`} />;
-        })}
-        {thumbnail.length > 2 && (
-          <span className="opacity-20 font-black text-[32px] top-[5px] font-Pretendard">
-            ...
-          </span>
-        )}
-      </div>
-    );
-  }
-  console.log(thumbnail);
+const CharacterItem = ({ name, time, content, characterImg }) => {
+  const ModifyCharacterImgg = () =>
+    React.cloneElement(characterImg(), {
+      className: 'h-full max-h-[180px] w-auto',
+    });
   return (
     <div
-      className={cx('relative max-w-[320px] xl:max-w-[270px]', {
-        'min-h-[320px] md:min-h-[380px]': thumbnail === undefined,
-        'min-h-[420px]': thumbnail !== undefined,
-      })}
+      className={cx(
+        'relative max-w-[320px] xl:max-w-[270px] min-h-[320px] md:min-h-[320px]',
+      )}
     >
       <div className="relative z-10 px-[36px] py-[34px] bg-[#f4f6fa] rounded-[30px]">
         <div className="text-[30px] font-extrabold text-left font-TmoneyRoundWind leading-none">
@@ -71,9 +37,10 @@ const CharacterItem = ({ name, time, content, thumbnail, characterImg }) => {
         <div className="mt-[20px] line-clamp-2 text-[14px] leading-[1.36] font-TmoneyRoundWind font-normal">
           {content()}
         </div>
-        <Thumbnails />
       </div>
-      <div className="absolute bottom-0 right-0 z-10">{characterImg()}</div>
+      <div className="absolute bottom-0 right-0 z-10">
+        <ModifyCharacterImgg />
+      </div>
     </div>
   );
 };
