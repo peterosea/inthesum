1f4a9f232c30d8b61ee6b7c43c079e84fdf89842
Hansanghyeon <999@hyeon.pro>
2022-05-19 18:31:53 +0900

    feat: 제목이 2줄일때도 말풍선의 크기 변화없에 최소 높이값

--- src/components/Item/Character.jsx
+++ src/components/Item/Character.jsx
@@ -18,21 +18,23 @@ const CharacterItem = ({ name, time, content, characterImg }) => {
   return (
     <div
       className={cx(
-        'relative max-w-[320px] xl:max-w-[270px] min-h-[320px] md:min-h-[320px]',
+        'relative max-w-[320px] xl:max-w-[270px] min-h-[390px] md:min-h-[320px]',
       )}
     >
-      <div className="relative z-10 px-[36px] py-[34px] bg-[#f4f6fa] rounded-[30px]">
-        <div className="text-[30px] font-extrabold text-left font-TmoneyRoundWind leading-none">
-          {name}
-          <span className="w-[39px] relative inline-block top-[6px] mr-[5px]">
-            <img
-              src="/img/new@3x.png"
-              srcSet="/img/new@2x.png 2x,
-                  /img/new@3x.png 3x"
-              alt="new"
-            />
-          </span>
-          <span className="text-[14px] font-normal text-[#999]">{time}</span>
+      <div className="relative z-10 p-[28px] sm:p-[34px] bg-[#f4f6fa] rounded-[30px] min-h-[230px]">
+        <div className="text-[30px] font-extrabold text-left font-TmoneyRoundWind leading-none flex flex-wrap items-end">
+          <div className="break-all line-clamp-2">{name}</div>
+          <div className="flex items-end">
+            <div className="w-[39px] mr-[5px]">
+              <img
+                src="/img/new@3x.png"
+                srcSet="/img/new@2x.png 2x,
+                    /img/new@3x.png 3x"
+                alt="new"
+              />
+            </div>
+            <div className="text-[14px] font-normal text-[#999]">{time}</div>
+          </div>
         </div>
         <div className="mt-[20px] line-clamp-2 text-[14px] leading-[1.36] font-TmoneyRoundWind font-normal">
           {content()}
