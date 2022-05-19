d987640ffd6a0342d566f05242bbfff06127b0f4
Hansanghyeon <999@hyeon.pro>
2022-05-19 21:07:08 +0900

    feat: 상세페이지 캐릭터 배너 스타일 수정

--- src/components/CharacterBanner.jsx
+++ src/components/CharacterBanner.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import classnames from 'classnames';
+import cx from 'classnames';
 
 const CharacterBanner = () => {
   return (
-    <div className="sm:rounded-[12px] relative px-cpx xl:px-[50px] py-[25px] bg-[#f4f6fa] grid grid-cols-[1fr,auto]">
+    <div className="sm:rounded-[12px] relative px-cpx xl:px-[50px] py-[25px] gap-[20px] bg-[#f4f6fa] grid xs:grid-cols-[1fr,auto]">
       <div className="self-center">
         <div className="inline-block relative pr-[20px]">
           <span
-            className={classnames(
+            className={cx(
               'text-[30px] font-bold font-TmoneyRoundWind text-black break-all',
               'xl:text-[42px]',
             )}
@@ -24,7 +24,7 @@ const CharacterBanner = () => {
           </span>
         </div>
         <div
-          className={classnames(
+          className={cx(
             'text-[#9ba0a8] text-[14px] font-Pretendard mt-[9px]',
             'xl:text-[20px] xl:mt-[15px]',
           )}
@@ -32,16 +32,16 @@ const CharacterBanner = () => {
           2022년 4월 13일 오후 5시 53분
         </div>
       </div>
-      <div className="self-end #lg:mt-[20px] relative">
+      <div className="self-end #lg:mt-[20px] relative #xs:justify-self-start">
         <img
-          className="h-[180px] absolute z-10 top-0 right-[76px]"
+          className="absolute z-10 top-0 right-[76px] h-full max-h-[135px] md:max-h-[180px] w-auto"
           src="/img/jung-kook.png"
           srcSet="/img/jung-kook@2x.png 2x,
                   /img/jung-kook@3x.png 3x"
           alt="JungKook"
         />
         <img
-          className="h-[180px]"
+          className="h-full max-h-[135px] md:max-h-[180px] w-auto pl-[76px]"
           src="/img/rm2.png"
           srcSet="/img/rm2@2x.png 2x,
                   /img/rm2@3x.png 3x"
