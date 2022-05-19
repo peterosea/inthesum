99ef9a0edfd4669175970956c5501052f8de8cd3
Hansanghyeon <999@hyeon.pro>
2022-05-19 20:57:48 +0900

    feat: 의상실 배너 480까지 2컬럼

--- src/pages/home/section4.jsx
+++ src/pages/home/section4.jsx
@@ -16,73 +16,6 @@ const ResizePlugin = (slider) => {
   });
 };
 
-export const PDBanner = () => {
-  const [loaded, setLoaded] = useState(false);
-  const [sliderRef, instanceRef] = useKeenSlider(
-    {
-      loop: true,
-      created() {
-        setLoaded(true);
-      },
-    },
-    [ResizePlugin],
-  );
-
-  return (
-    <div className="relative md:rounded-[12px] h-full overflow-hidden">
-      <div ref={sliderRef} className="keen-slider h-full">
-        {[...Array(2)].map((_, index) => (
-          <a
-            key={`index-${index}`}
-            href="#"
-            className={classnames(
-              'keen-slider__slide',
-              'relative flex py-[42px] px-[30px] bg-[#EDEDFD] items-center flex-wrap gap-y-[35px] justify-end',
-              'md:justify-between md:grid md:grid-cols-[290px,170px] md:gap-x[40px] md:p-[50px]',
-              'xl:rounded-[12px] xl:pl-[60px] xl:pr-[40px]',
-            )}
-          >
-            <div className="#xl:w-full">
-              <h4 className="text-[#2e0064] font-extrabold font-TmoneyRoundWind">
-                개발 PD 노트
-              </h4>
-              <div className="mt-[8px] text-[22px] text-primary font-extrabold font-TmoneyRoundWind tracking-[-1.1px]">
-                인더섬 with BTS에서
-                <br />
-                여러분께 전하고 싶은 이야기
-              </div>
-              <div className="text-[#39007c] mt-[20px] font-Pretendard  tracking-[-0.8px]">
-                여러분과의 이야기를 만들어가고 싶습니다.
-                <br />
-                PD가 직접 들려주는 인더섬 이야기를 만나보세요!
-              </div>
-            </div>
-            <img
-              className="h-[150px] w-auto"
-              src="/img/banner-pd-note-img@3x.png"
-              alt=""
-            />
-          </a>
-        ))}
-      </div>
-      {loaded && (
-        <div className="absolute top-[18px] right-[18px] flex gap-x-[3px]">
-          <button
-            className="rotate-180"
-            onClick={(e) => e.stopPropagation() || instanceRef.current?.prev()}
-          >
-            <img src="/img/3716.svg" alt="" className="!w-auto" />
-          </button>
-          <button
-            onClick={(e) => e.stopPropagation() || instanceRef.current?.next()}
-          >
-            <img src="/img/3716.svg" alt="" className="!w-auto" />
-          </button>
-        </div>
-      )}
-    </div>
-  );
-};
 
 export const ClothesBanner = () => {
   const [loaded, setLoaded] = useState(false);
@@ -105,8 +38,9 @@ export const ClothesBanner = () => {
             href="#"
             className={classnames(
               'keen-slider__slide',
-              'relative flex py-[42px] px-[30px] bg-[#FDF4FF] items-center flex-wrap gap-y-[35px] justify-end',
-              'md:justify-between md:grid md:grid-cols-[290px,170px] md:gap-x[40px] md:p-[50px]',
+              'relative flex py-[42px] px-[30px] bg-[#FDF4FF] items-center flex-wrap gap-y-[35px]',
+              'xs:grid-cols-[minmax(auto,290px),minmax(auto,170px)] xs:grid xs:gap-x-[20px] xs:p-[50px] xs:justify-between',
+              'md:gap-x-[40px]',
               'xl:rounded-[12px] xl:pl-[60px] xl:pr-[40px]',
             )}
           >
@@ -123,7 +57,7 @@ export const ClothesBanner = () => {
                 멋진 의상들로 곧 찾아뵐꼐요.
               </div>
             </div>
-            <div className="relative">
+            <div className="relative self-end mt-[40px]">
               <img
                 src="/img/3706.png"
                 srcSet="/img/3706@2x.png 2x,
